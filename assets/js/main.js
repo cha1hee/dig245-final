386588908524b4f74925fc24e4ac088787696cca
Pauline Cha <pacha@davidson.edu>
2022-11-16 13:42:47 -0500

    JavaScript separate file, transitioning through pop ups

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
+document.getElementById('start').addEventListener('click', function() {
+  let popup = document.getElementById("q1");
+  let prev = document.getElementById("start");
+    popup.style.visibility = "visible";
+    prev.style.visibility = "hidden";
+  })
 
+$('#scam1, #real1').click(function() {
+  let popup = document.getElementById("q2");
+  let prev = document.getElementById("q1");
+    popup.style.visibility = "visible";
+    prev.style.visibility = "hidden";
+  })
+
+$('#scam2, #real2').click(function() {
+  let popup = document.getElementById("q3");
+  let prev = document.getElementById("q2");
+    popup.style.visibility = "visible";
+    prev.style.visibility = "hidden";
+  })
